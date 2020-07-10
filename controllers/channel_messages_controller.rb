f66816b36abe2863dbdbeec3d02e1f9365ba46d6
kaiomagalhaes <me@kaiomagalhaes.com>
2020-07-10 17:25:02 -0300

    add link names to channel messages

--- controllers/channel_messages_controller.rb
+++ controllers/channel_messages_controller.rb
@@ -3,13 +3,13 @@ require 'slack-ruby-client'
 
 class ChannelMessagesController < ApplicationController
   def create!
-    response = client.chat_postMessage(channel: formatted_channel, text: message, thread_ts: ts)
+    response = client.chat_postMessage(channel: formatted_channel, text: message, thread_ts: ts, link_names: true)
 
     respond!(response)
   end
 
   def update!
-    response = client.chat_update(channel: formatted_channel, text: message, ts: ts)
+    response = client.chat_update(channel: formatted_channel, text: message, ts: ts, link_names: true)
 
     respond!(response)
   end
