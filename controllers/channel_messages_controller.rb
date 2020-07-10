b5ac4ef7f543f277c0af323840c21c720fafc8d2
kaiomagalhaes <me@kaiomagalhaes.com>
2020-07-10 09:45:16 -0300

    fix thread messaging on create message

--- controllers/channel_messages_controller.rb
+++ controllers/channel_messages_controller.rb
@@ -3,13 +3,13 @@ require 'slack-ruby-client'
 
 class ChannelMessagesController < ApplicationController
   def create!
-    response = client.chat_postMessage(channel: formatted_channel, text: message)
+    response = client.chat_postMessage(channel: formatted_channel, text: message, thread_ts: ts)
 
     respond!(response)
   end
 
   def update!
-    response = client.chat_update(channel: formatted_channel, text: message, ts: @params[:ts])
+    response = client.chat_update(channel: formatted_channel, text: message, ts: ts)
 
     respond!(response)
   end
@@ -18,5 +18,9 @@ class ChannelMessagesController < ApplicationController
 
   def formatted_channel
     "\##{@params[:channel]}"
+  end
+
+  def ts
+    @params[:ts]
   end
 end
