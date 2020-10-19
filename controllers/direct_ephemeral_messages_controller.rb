034f29a8783b9e1eec191634c9288d2f35d95043
Rheniery <rhenierymendes@gmail.com>
2020-10-19 17:02:42 -0300

    Change ephemeral messages controller to accept channel

--- controllers/direct_ephemeral_messages_controller.rb
+++ controllers/direct_ephemeral_messages_controller.rb
@@ -2,10 +2,13 @@ require './controllers/application_controller'
 require 'slack-ruby-client'
 
 class DirecEphemeraltMessagesController < ApplicationController
+
+  DEFAULT_PRIVATE_MESSAGE_CHANNEL = 'directmessage'.freeze
+
   def create!
     response = client.chat_postEphemeral(
       user: formatted_username,
-      channel: formatted_username,
+      channel: channel,
       text: message,
       blocks: blocks
     )
@@ -17,6 +20,14 @@ class DirecEphemeraltMessagesController < ApplicationController
 
   def formatted_username
     "@#{@params[:username]}"
+  end
+
+  def channel
+    channel_param.to_s.strip.empty? || channel_param == DEFAULT_PRIVATE_MESSAGE_CHANNEL ? formatted_username : channel_param
+  end
+
+  def channel_param
+    @params[:channel]
   end
 
   def blocks
